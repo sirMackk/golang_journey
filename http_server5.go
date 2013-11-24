ecf22bcee5e5a903f43253244d5c527230724b82
sirMackk <c.tairix@gmail.com>
2013-11-24 19:07:29 +0000

    Adds basic crud index function

--- http_server5.go
+++ http_server5.go
@@ -3,17 +3,19 @@ package main
 import (
     "net/http"
     "log"
-    "os"
-    "strings"
-    "io/ioutil"
+    //"os"
+    //"strings"
+    //"io/ioutil"
     "fmt"
-    "html/template"
-    "runtime"
+    //"html/template"
+    //"runtime"
+    "github.com/sirMackk/templates_ago"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 type Record struct {
-  Name string
-  Id int
+   Title, Body, Fs_path string 
 }
 
 type HandleFunc func(w http.ResponseWriter, req *http.Request)
@@ -42,56 +44,88 @@ func HandleWrapper(function HandleFunc) HandleFunc {
 
 func Index(w http.ResponseWriter, req *http.Request) {
     //rec := &Record{Id: 1, Name: "Bob"}
-    rec := make(map[string]interface{})
-    rec["Name"] = "Main Index"
-    rec["Records"] = []string{"this", "is", "sparta"}
-    fmt.Println(getFuncName())
-    //err := templates.ExecuteTemplate(w, "index.html", rec)
-    //err := templates["index.html"].Execute(w, rec)
-    
-    err := templates[getFuncName() + ".html"].ExecuteTemplate(w, "base", rec)
+    //rec := make(map[string]interface{})
+    //rec["Name"] = "Main Index"
+    //rec["Records"] = []string{"this", "is", "sparta"}
+    //recs := make([]map[string]string)
+    recs := make([]Record, 0)
+    rows, err := db.Query("select title, body, fs_path from Records limit 10")
+    //cols, err := rows.Columns()
+    if err != nil {
+        fmt.Println(52)
+        fmt.Println(err)
+        panic(err)
+    }
+    defer rows.Close()
+    for rows.Next() {
+        //vals := make([]interface{}, len(cols))
+        //rows.Scan(vals...)
+        var title, body, fs_path string
+        rows.Scan(&title, &body, &fs_path)
+        //recs = append(&Record{Title: string(vals[0]), Body: vals[1], Fs_path: vals[2]})
+        recs = append(recs, Record{Title: title, Body: body, Fs_path: fs_path})
+    }
+
+
+    err = templates.Execute(w, recs)
     if err != nil { panic(err) }
-    //t, _ := template.ParseFiles("index.html")
-    //t.Execute(w, rec)
 }
 
 func SimplePage(w http.ResponseWriter, req *http.Request) {
     fmt.Fprint(w, "Simple routing test")
 }
 
-func getFuncName() string {
-    pc, _, _, ok := runtime.Caller(1)
-    if !ok { return "Unknown" }
-    fnName := runtime.FuncForPC(pc)
-    if fnName == nil { return "Anonymous" }
-    fnNameParts := strings.Split(fnName.Name(), ".")
-    return strings.ToLower(fnNameParts[len(fnNameParts)-1])
-}
+var db *sql.DB
 
-
-
-func setupTemplates() {
-    tplPath := "templates/"
-    if _, err := os.Stat("templates/base.html"); os.IsNotExist(err) {
-        panic("Missing templates/base.html file")
-    }
-    files, err := ioutil.ReadDir(tplPath)
+func setupDatabase() {
+    var err error
+    db, err = sql.Open("sqlite3", "./test_db.db")
     if err != nil { panic(err) }
-    for _, file := range files {
-        if file.Name() == "base.html" { continue }
-        fmt.Println(file.Name())
-        templates[file.Name()] = template.Must(template.ParseFiles(tplPath + file.Name(), "base.html"))
+
+    creation := `
+    create table if not exists Records(id integer not null primary key, title text, body text, fs_path text);
+    create table if not exists Users(id integer not null primary key, username text, password text, role text);
+    `
+
+    _, err = db.Exec(creation)
+    if err != nil {
+        fmt.Println("85")
+        panic(err)
     }
+
+    tx, err := db.Begin()
+    if err != nil { 
+        fmt.Println("91")
+        panic(err)
+    }
+
+    stmt, err := tx.Prepare("insert into Records(title, body, fs_path) values(?, ?, ?)")
+    if err != nil { 
+      fmt.Println(97)
+      fmt.Println(err)
+    }
+    defer stmt.Close()
+
+    for i := 0; i < 10; i++ {
+        _, err = stmt.Exec(fmt.Sprintf("Title %d", i), "Body", "./")
+        if err != nil {
+            fmt.Println("stmt error")
+        }
+    }
+
+    tx.Commit()
 }
 
-//var templates map[string]*template.Template
-var templates = make(map[string]*template.Template)
+
+var templates = templates_ago.NewTemplates()
 
 func init() {
-    setupTemplates()
+    setupDatabase()
+    templates_ago.LoadTemplates("templates/", templates)
 }
 
 func main() {
+    defer db.Close()
     http.HandleFunc("/", HandleWrapper(Index))
     http.HandleFunc("/other/", HandleWrapper(SimplePage))
 
